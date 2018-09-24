9d37ee9c04951253fbf3fe4aacbb853d0c383934
Ian Boswell <iancboswell@gmail.com>
2018-09-24 17:04:00 -0400

    Struct success

--- main.go
+++ main.go
@@ -27,6 +27,7 @@ func main() {
 
 	InsertSQLX(db)
 	InsertSquirrel(db)
+	InsertStructMember(db)
 }
 
 func InsertSQLX(db *sqlx.DB) {
@@ -48,7 +49,7 @@ func InsertSQLX(db *sqlx.DB) {
 		log.Fatalf("Introspection failed: %v", err)
 	}
 
-	ret := wkb.Point{Point: geom.NewPoint(geom.XY)}
+	var ret wkb.Point
 	err = db.Get(&ret, "SELECT ST_AsWKB(pt) FROM points WHERE id = ?", lastID)
 	if err != nil {
 		log.Fatalf("SELECT failed: %v", err)
@@ -83,7 +84,7 @@ func InsertSquirrel(db *sqlx.DB) {
 		log.Fatalf("SQL introspection failed: %v", err)
 	}
 
-	ret := wkb.Point{Point: geom.NewPoint(geom.XY)}
+	var ret wkb.Point
 
 	query, args, err = sq.Select("ST_AsWKB(pt)").From("points").
 		Where(sq.Eq{"id": lastID}).
@@ -98,4 +99,51 @@ func InsertSquirrel(db *sqlx.DB) {
 	}
 
 	log.Printf("Retrieved point with coordinates:\n(%v, %v)\n", ret.FlatCoords()[0], ret.FlatCoords()[1])
+}
+
+func InsertStructMember(db *sqlx.DB) {
+	type Pointy struct {
+		P wkb.Point `db:"ST_AsWKB(pt)"`
+	}
+
+	// Define a point in 2-D space as a struct member.
+	p := Pointy{P: wkb.Point{
+		Point: geom.NewPoint(geom.XY).
+			MustSetCoords(
+				geom.Coord{-77.8187259, 40.8089934})}}
+
+	log.Printf("Inserting point with coordinates:\n(%v, %v)\n", p.P.FlatCoords()[0], p.P.FlatCoords()[1])
+
+	query, args, err := sq.Insert("").Into("points").Columns("pt").
+		Values(sq.Expr("ST_GeomFromWKB(?)", &p.P)).
+		ToSql()
+	if err != nil {
+		log.Fatalf("SQL generation failed: %v", err)
+	}
+
+	res, err := db.Exec(query, args...)
+	if err != nil {
+		log.Fatalf("SQL insert execution error: %v", err)
+	}
+
+	lastID, err := res.LastInsertId()
+	if err != nil {
+		log.Fatalf("SQL introspection failed: %v", err)
+	}
+
+	var ret Pointy
+
+	query, args, err = sq.Select("ST_AsWKB(pt)").From("points").
+		Where(sq.Eq{"id": lastID}).
+		ToSql()
+	if err != nil {
+		log.Fatalf("SQL generation failed: %v", err)
+	}
+
+	err = db.Get(&ret, query, args...)
+	if err != nil {
+		log.Fatalf("SELECT failed: %v", err)
+	}
+
+	log.Printf("Retrieved point with coordinates:\n(%v, %v)\n", ret.P.FlatCoords()[0], ret.P.FlatCoords()[1])
 }
